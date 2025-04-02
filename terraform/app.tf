43ab12a31b034996197d231d4b2562d0cac18166
Ryan Ahearn <ryan.ahearn@gsa.gov>
2025-04-02 08:25:20 -0400

    Initial commit

---
+++ terraform/app.tf
@@ -0,0 +1,63 @@
+data "archive_file" "src" {
+  type        = "zip"
+  source_dir  = "${path.module}/.."
+  output_path = "${path.module}/dist/src.zip"
+  excludes = [
+    ".git*",
+    ".circleci/*",
+    ".bundle/*",
+    "node_modules/*",
+    "tmp/**/*",
+    "terraform/*",
+    "log/*",
+    "doc/*"
+  ]
+}
+
+locals {
+  host_name = coalesce(var.host_name, "${local.app_name}-${var.env}")
+  domain    = coalesce(var.custom_domain_name, "app.cloud.gov")
+}
+
+resource "cloudfoundry_app" "app" {
+  name       = "${local.app_name}-${var.env}"
+  space_name = var.cf_space_name
+  org_name   = local.cf_org_name
+
+  path             = data.archive_file.src.output_path
+  source_code_hash = data.archive_file.src.output_base64sha256
+  buildpacks       = ["ruby_buildpack"]
+  strategy         = "rolling"
+  routes           = [{ route = "${local.host_name}.${local.domain}" }]
+
+  environment = {
+    RAILS_ENV                = var.env
+    RAILS_MASTER_KEY         = var.rails_master_key
+    RAILS_LOG_TO_STDOUT      = "true"
+    RAILS_SERVE_STATIC_FILES = "true"
+  }
+
+  processes = [
+    {
+      type                       = "web"
+      instances                  = var.web_instances
+      memory                     = var.web_memory
+      health_check_http_endpoint = "/up"
+      health_check_type          = "http"
+      command                    = "./bin/rake cf:on_first_instance db:migrate && exec env HTTP_PORT=$PORT ./bin/thrust ./bin/rails server"
+    }
+  ]
+
+  service_bindings = [
+
+
+    { service_instance = "${local.app_name}-rds-${var.env}" }
+  ]
+
+  depends_on = [
+
+
+    module.app_space,
+    module.database
+  ]
+}
---
+++ terraform/main.tf
@@ -0,0 +1,50 @@
+locals {
+  cf_org_name     = "gsa-tts-devtools-prototyping"
+  app_name        = "saml_proxy"
+  space_deployers = setunion([var.cf_user], var.space_deployers)
+}
+
+
+module "app_space" {
+  source = "github.com/gsa-tts/terraform-cloudgov//cg_space?ref=v2.1.0"
+
+  cf_org_name          = local.cf_org_name
+  cf_space_name        = var.cf_space_name
+  allow_ssh            = var.allow_space_ssh
+  deployers            = local.space_deployers
+  developers           = var.space_developers
+  security_group_names = ["trusted_local_networks_egress"]
+}
+
+
+module "database" {
+  source = "github.com/gsa-tts/terraform-cloudgov//database?ref=v2.1.0"
+
+  cf_space_id   = module.app_space.space_id
+  name          = "${local.app_name}-rds-${var.env}"
+  rds_plan_name = var.rds_plan_name
+  # depends_on line is required only for initial creation and destruction. It can be commented out for updates if you see unwanted cascading effects
+  depends_on = [module.app_space]
+}
+
+
+
+###########################################################################
+# Before setting var.custom_domain_name, perform the following steps:
+# 1) Domain must be manually created by an OrgManager:
+#     cf create-domain var.cf_org_name var.domain_name
+# 2) ACME challenge record must be created.
+#     See https://cloud.gov/docs/services/external-domain-service/#how-to-create-an-instance-of-this-service
+###########################################################################
+module "domain" {
+  count  = (var.custom_domain_name == null ? 0 : 1)
+  source = "github.com/gsa-tts/terraform-cloudgov//domain?ref=v2.1.0"
+
+  cf_org_name   = local.cf_org_name
+  cf_space      = module.app_space.space
+  cdn_plan_name = "domain"
+  domain_name   = var.custom_domain_name
+  host_name     = var.host_name
+  # depends_on line is required only for initial creation and destruction. It can be commented out for updates if you see unwanted cascading effects
+  depends_on = [module.app_space]
+}
